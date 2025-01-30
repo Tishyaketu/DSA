cec02e5bc86e5df01fe5223a0036d55034b5a1d4
Tishyaketu Deshpande <24838965+Tishyaketu@users.noreply.github.com>
2025-01-30 11:54:30 -0800

    Time: 25 ms (5.20%) | Memory: 17.8 MB (30.72%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x<0: return False
+        stack = []
+        while x!=0:
+          y = x%10
+          stack.append(y)
+          x = x//10
+        return stack==stack[::-1]
