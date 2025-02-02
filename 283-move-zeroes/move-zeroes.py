806252feec4b57bd5a1c6f4efbb9cc36a06241c9
Tishyaketu Deshpande <24838965+Tishyaketu@users.noreply.github.com>
2025-02-01 21:58:43 -0800

    Time: 7 ms (47.31%) | Memory: 18.7 MB (75.42%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -2,6 +2,12 @@ class Solution:
     def moveZeroes(self, nums: list[int]) -> None:
         last_non_zero_found_at = 0
         for cur in range(len(nums)):
+            # print('cur: ',cur)
+            # print('last_non_zero_found_at: ',last_non_zero_found_at)
             if nums[cur] != 0:
+                # print('nums[',cur,']',nums[cur])
+                # print('nums[',last_non_zero_found_at,']',nums[last_non_zero_found_at])
                 nums[last_non_zero_found_at], nums[cur] = nums[cur], nums[last_non_zero_found_at]
                 last_non_zero_found_at += 1
+                # print('nums: ',nums)
+                # print('last_non_zero_found_at: ',last_non_zero_found_at)
