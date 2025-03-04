58288844c3b737a776151d044cc23ba8b92bd6db
Tishyaketu Deshpande <24838965+Tishyaketu@users.noreply.github.com>
2025-03-04 10:38:56 -0800

    Time: 40 ms (52.18%) | Memory: 17.7 MB (55.28%) - LeetSync

--- 190-reverse-bits/reverse-bits.py
+++ 190-reverse-bits/reverse-bits.py
@@ -1,8 +1,9 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        ret, power = 0, 31
-        while n:
-          ret += (n&1)<<power
-          n = n>>1
-          power -= 1
-        return ret
+        res = 0 # this is the result that would be returned initially zero
+        power = 31 # this is used to repsresent 32-bit numbers bits starting from 0 to 31
+        while n: # to travrse allthe bits of 32-bit input integer
+          res += (n&1)<<power # n&1 takes the rightmost bit, <<power left shifts that bit to its corresponding leftmost place
+          n = n>>1 # right shift the input by 1 bit to process next bit
+          power -= 1 # reduce the power to place next bit in its corresponding place
+        return res #return the result
