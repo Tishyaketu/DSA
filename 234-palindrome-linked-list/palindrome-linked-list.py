f402738fe64991a83265866d787f0daebb753333
Tishyaketu Deshpande <24838965+Tishyaketu@users.noreply.github.com>
2025-03-23 15:12:49 -0700

    Time: 42 ms (32.68%) | Memory: 34.8 MB (63.04%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -4,29 +4,28 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-  def isPalindrome(self, head: Optional[ListNode]) -> bool:
-    if head is None: return True
-    middle = self.middle(head)
-    rev_sh = self.rev_sh(middle.next)
-    fp = head
-    sp = rev_sh
-    while sp:
-      if fp.val!=sp.val: return False
-      fp = fp.next
-      sp = sp.next
-    return True
-  def middle(self,node):
-    slow, fast = node, node
-    while fast.next and fast.next.next:
-      slow = slow.next
-      fast = fast.next.next
-    return slow
-  def rev_sh(self,node):
-    prev = None
-    curr = node
-    while curr:
-      nn = curr.next
-      curr.next = prev
-      prev = curr
-      curr = nn
-    return prev    
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        middle = self.middle(head)
+        rev_sh= self.rev_sh(middle.next)
+        fp = head
+        sp = rev_sh
+        while sp:
+          if fp.val!=sp.val: return False
+          fp = fp.next
+          sp = sp.next
+        return True
+    def middle(self,node):
+      slow, fast = node, node
+      while fast.next and fast.next.next:
+        slow = slow.next
+        fast = fast.next.next
+      return slow
+    def rev_sh(self,node):
+      prev = None
+      curr = node
+      while curr:
+        nn = curr.next
+        curr.next = prev
+        prev = curr
+        curr = nn
+      return prev
