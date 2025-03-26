6ae37d4a93102c4046cd6232091993382232b77f
Tishyaketu Deshpande <24838965+Tishyaketu@users.noreply.github.com>
2025-03-25 18:59:21 -0700

    Time: 87 ms (81.78%) | Memory: 26.5 MB (96.88%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -2,14 +2,14 @@ class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         n = len(temperatures)
         hottest = 0
-        waiting_days_array = [0] * n
-        for cd in range(n - 1, -1, -1):
-            ct = temperatures[cd]
-            if ct >= hottest:
-                hottest = ct
-                continue
-            waiting_days = 1
-            while temperatures[cd + waiting_days] <= ct:
-                waiting_days += waiting_days_array[cd + waiting_days]
-            waiting_days_array[cd] = waiting_days
-        return waiting_days_array
+        answer = [0]*n
+        for i in range(n-1,-1,-1):
+          ct = temperatures[i]
+          if ct>=hottest:
+            hottest = ct
+            continue
+          waiting_days = 1
+          while temperatures[i+waiting_days]<=ct:
+            waiting_days += answer[i+waiting_days]
+          answer[i] = waiting_days
+        return answer
