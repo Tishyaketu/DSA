726a903fafd89fc3c1db45f0e00604c54ff73879
Tishyaketu Deshpande <24838965+Tishyaketu@users.noreply.github.com>
2025-03-30 14:52:15 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (18.89%) - LeetSync

--- 408-valid-word-abbreviation/valid-word-abbreviation.py
+++ 408-valid-word-abbreviation/valid-word-abbreviation.py
@@ -1,21 +1,20 @@
 class Solution:
     def validWordAbbreviation(self, word: str, abbr: str) -> bool:
-        word_index = 0
-        abbr_index = 0
-        word_length = len(word)
-        abbr_length = len(abbr)
-        while word_index < word_length and abbr_index<abbr_length:
-            if word[word_index] == abbr[abbr_index]:
-                word_index += 1
-                abbr_index += 1
-                continue
-            if not abbr[abbr_index].isdigit():
-                return False
-            num_start_index = abbr_index
-            while abbr_index < abbr_length and abbr[abbr_index].isdigit():
-                abbr_index += 1
-            num_str = abbr[num_start_index:abbr_index]
-            if num_str[0] == '0':
-                return False
-            word_index += int(num_str)
-        return word_index == word_length and abbr_index == abbr_length
+        w_l = len(word)
+        a_l = len(abbr)
+        w_i, a_i = 0, 0
+        while w_i<w_l and a_i<a_l:
+          if word[w_i]==abbr[a_i]:
+            w_i += 1
+            a_i += 1
+            continue
+          if not abbr[a_i].isdigit():
+            return False
+          n_s_i = a_i
+          while a_i<a_l and abbr[a_i].isdigit():
+            a_i += 1
+          n_s = abbr[n_s_i:a_i]
+          if n_s[0]=='0':
+            return False
+          w_i += int(n_s)
+        return w_i==w_l and a_i==a_l
