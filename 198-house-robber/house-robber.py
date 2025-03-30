488ac340ca6059a2a715545d916db750944dc2af
Tishyaketu Deshpande <24838965+Tishyaketu@users.noreply.github.com>
2025-03-30 16:28:47 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (34.19%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -2,13 +2,13 @@ class Solution:
     def __init__(self):
       self.memo = {}
     def rob(self, nums: List[int]) -> int:
-        # find max value, add to res
-        # keep on adding non-adajacent left and right nbrs to res
         self.memo = {}
         return self.robFrom(0,nums)
     def robFrom(self,i,nums):
-      if i>=len(nums): return 0
-      if i in self.memo: return self.memo[i]
-      ans = max(self.robFrom(i+1,nums),nums[i]+self.robFrom(i+2,nums))
+      if i>=len(nums):
+        return 0
+      if i in self.memo: 
+        return self.memo[i]
+      ans = max(nums[i]+self.robFrom(i+2,nums),self.robFrom(i+1,nums))
       self.memo[i] = ans
       return ans
