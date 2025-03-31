846566d8d0d0fe05eea53a19f85c9e28e2324578
Tishyaketu Deshpande <24838965+Tishyaketu@users.noreply.github.com>
2025-03-30 17:57:09 -0700

    Time: 59 ms (68.83%) | Memory: 17.9 MB (94.43%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -1,21 +1,16 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        def check_palindrome(s, i, j):
-            while i < j:
-                if s[i] != s[j]:
-                    return False
-                i += 1
-                j -= 1
-            
-            return True
-
-        i = 0
-        j = len(s) - 1
-        while i < j:
-            # Found a mismatched pair - try both deletions
-            if s[i] != s[j]:
-                return check_palindrome(s, i, j - 1) or check_palindrome(s, i + 1, j)
-            i += 1
-            j -= 1
-        
-        return True
+      def cPal(s,i,j):
+        while i<j:
+          if s[i] != s[j]:
+            return False
+          i+=1
+          j-=1
+        return True  
+      i, j = 0, len(s)-1
+      while i<j:
+        if s[i]!=s[j]:
+          return cPal(s,i+1,j) or cPal(s,i,j-1)
+        i += 1
+        j -= 1
+      return True      
