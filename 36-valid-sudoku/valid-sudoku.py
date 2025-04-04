c6fcfa1799c3720c221674fb76b749183fada51f
Tishyaketu Deshpande <24838965+Tishyaketu@users.noreply.github.com>
2025-04-04 14:12:35 -0700

    Time: 2 ms (87.64%) | Memory: 17.6 MB (92.22%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,11 +1,11 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        N = 9
-        rowsets = [set() for _ in range(N)]
-        colsets = [set() for _ in range(N)]
-        boxsets = [set() for _ in range(N)]
-        for i in range(N):
-          for j in range(N):
+        n = 9
+        rowsets = [set() for _ in range(n)]
+        colsets = [set() for _ in range(n)]
+        boxsets = [set() for _ in range(n)]
+        for i in range(n):
+          for j in range(n):
             element = board[i][j]
             if element!='.':
               if element in rowsets[i]:
@@ -14,9 +14,8 @@ class Solution:
               if element in colsets[j]:
                 return False
               colsets[j].add(element)
-              boxNum = ((i//3)*3)+j//3
+              boxNum = ((i//3)*3) + j//3
               if element in boxsets[boxNum]:
                 return False
               boxsets[boxNum].add(element)
-              
-        return True 
+        return True
