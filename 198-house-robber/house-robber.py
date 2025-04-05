4100a8b1d22e05711cc2d7b4e4429223b2580963
Tishyaketu Deshpande <24838965+Tishyaketu@users.noreply.github.com>
2025-04-04 19:04:10 -0700

    Time: 0 ms (100.00%) | Memory: 17.6 MB (82.62%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,13 +1,10 @@
 class Solution:
-    def __init__(self):
-      self.memo = {}
     def rob(self, nums: List[int]) -> int:
-        self.memo = {}
-        return self.robFrom(0,nums)
-    def robFrom(self,i,nums):
-      if i>=len(nums):
-        return 0
-      if i in self.memo: 
-        return self.memo[i]
-      self.memo[i] = max(nums[i]+self.robFrom(i+2,nums),self.robFrom(i+1,nums))
-      return self.memo[i]
+      memo = {}
+      return self.robFrom(0,nums,memo)
+    def robFrom(self,start,nums,memo):
+      if start>=len(nums): return 0
+      if start in memo: return memo[start]
+      memo[start] = max(nums[start]+self.robFrom(start+2,nums,memo),self.robFrom(start+1,nums,memo))
+      return memo[start]
+        
