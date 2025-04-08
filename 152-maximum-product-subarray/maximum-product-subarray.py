d867ab52e262f083215299964edd5d9e1dca5d4f
Tishyaketu Deshpande <24838965+Tishyaketu@users.noreply.github.com>
2025-04-07 18:09:21 -0700

    Time: 226 ms (5.01%) | Memory: 68.8 MB (5.44%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.py
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -1,14 +1,14 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         self.memo = {}
-        def dfs(i, currentProduct):
-            key = (i, currentProduct)
+        return self.dfs(1, nums[0],nums)
+    def dfs(self, i, previousProduct,nums):
+            key = (i, previousProduct)
             if (key in self.memo):
                 return self.memo[key]
             if (i >= len(nums)):
-                return currentProduct
+                return previousProduct
 			# 3 choices, Include the current number in the product, start a new product, or end the product
-            ans = max(dfs(i + 1, currentProduct * nums[i]), dfs(i + 1, nums[i]), currentProduct)
+            ans = max(self.dfs(i + 1, previousProduct * nums[i],nums), self.dfs(i + 1, nums[i],nums), previousProduct)
             self.memo[key] = ans
             return ans
-        return dfs(1, nums[0])
