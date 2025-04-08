84363f3a03048876f750f28385d966057fe580c1
Tishyaketu Deshpande <24838965+Tishyaketu@users.noreply.github.com>
2025-04-07 18:46:09 -0700

    Time: 235 ms (5.01%) | Memory: 68.6 MB (5.44%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.py
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -1,11 +1,10 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         self.memo = {}
-        n = len(nums)
-        return self.calculate(1,nums[0],n,nums)
-    def calculate(self,i,prevProd,n,nums):
-        if i>=n: return prevProd
+        return self.recurse(1,nums[0],nums)
+    def recurse(self,i,prevProd,nums):
+        if i>=len(nums): return prevProd
         key = (i, prevProd)
         if key in self.memo: return self.memo[key]
-        self.memo[key] = max(self.calculate(i+1,prevProd*nums[i],n,nums),self.calculate(i+1,nums[i],n,nums),prevProd)
+        self.memo[key] = max(self.recurse(i+1,prevProd*nums[i],nums),self.recurse(i+1,nums[i],nums),prevProd)
         return self.memo[key]
