a9b3f7bd63cdca674b11fa7f64c328dcee0a7d4d
Tishyaketu Deshpande <24838965+Tishyaketu@users.noreply.github.com>
2025-05-04 18:31:05 -0700

    Time: 51 ms (24.85%) | Memory: 19 MB (99.52%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -2,13 +2,13 @@ class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         pq = deque([])
         aq = deque([])
-        nr, nc = len(heights), len(heights[0])
-        for i in range(nr):
+        numr, numc = len(heights), len(heights[0])
+        for i in range(numr):
           pq.append((i,0))
-          aq.append((i,nc-1))
-        for j in range(nc):
+          aq.append((i,numc-1))
+        for j in range(numc):
           pq.append((0,j))
-          aq.append((nr-1,j))
+          aq.append((numr-1,j))
         def bfs(q):
           reachable = set()
           while q:
@@ -16,7 +16,7 @@ class Solution:
             reachable.add((r,c))
             for (x,y) in [(1,0),(0,1),(-1,0),(0,-1)]:
               newr, newc = r+x, c+y
-              if newr<0 or newc>=nc or newc<0 or newr>=nr:
+              if newr<0 or newc>=numc or newc<0 or newr>=numr:
                 continue
               if (newr,newc) in reachable:
                 continue
