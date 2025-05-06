f673f52864abd498a5c7725c211f7b7abd358ab9
Tishyaketu Deshpande <24838965+Tishyaketu@users.noreply.github.com>
2025-05-06 15:02:33 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (12.85%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -5,17 +5,20 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        # corner cases adjust karay sathi dummy node at the beginning
         dummy = ListNode(0)
-        length = 0
         dummy.next = head
-        first = head
-        while first:
-          length+=1
-          first = first.next
-        length-=n
+        # have two pointers at dummy node
         first = dummy
-        while length>0:
-          length-=1
+        second = dummy
+        # move first pointer 'n' distance apart from second
+        for i in range(n+1):
           first = first.next
-        first.next = first.next.next
+        # move 1st pointer till null, while maintaining 'n' distance in between
+        while first:
+          first = first.next
+          second = second.next
+        # skip the required node i.e. 2nd.next.next
+        second.next = second.next.next
+        # return dummy.next i.e. original required LL
         return dummy.next
