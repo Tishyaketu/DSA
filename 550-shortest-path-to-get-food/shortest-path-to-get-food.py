ad7d252a74490269cb9ca9d453d9c767ffb1d845
Tishyaketu Deshpande <24838965+Tishyaketu@users.noreply.github.com>
2025-05-14 14:40:26 -0700

    Time: 50 ms (93.15%) | Memory: 24.5 MB (64.08%) - LeetSync

--- 550-shortest-path-to-get-food/shortest-path-to-get-food.py
+++ 550-shortest-path-to-get-food/shortest-path-to-get-food.py
@@ -1,24 +1,18 @@
 class Solution:
     def getFood(self, grid: List[List[str]]) -> int:
-        # start q from '*' set counter to
-        # if valid and 'O' push into q, increment counter
-        # if '#' then return counter val
-        # if q is empty return -1
         m, n = len(grid), len(grid[0])
         q = deque([])
         for i in range(m):
           for j in range(n):
             if grid[i][j]=='*':
               q.append((i,j,0))
-              grid[i][j] = 'X'
+              grid[i][j]='X'
         while q:
           r, c, s = q.popleft()
-          for (x,y) in [(1,0),(-1,0),(0,-1),(0,1)]:
+          for (x,y) in [(0,1),(1,0),(0,-1),(-1,0)]:
             newr, newc = r+x, c+y
-            if newr<0 or newc>=n or newc<0 or newr>=m:
-              continue
-            if grid[newr][newc]=='O': 
-              q.append((newr,newc,s+1))
-              grid[newr][newc] = 'X'
-            elif grid[newr][newc]=='#': return s+1
+            if newr<0 or newc>=n or newc<0 or newr>=m or grid[newr][newc]=='X': continue
+            if grid[newr][newc]=='#': return s+1
+            grid[newr][newc]='X'
+            q.append((newr,newc,s+1))
         return -1
