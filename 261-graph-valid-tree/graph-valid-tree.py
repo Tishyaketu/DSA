ff0e0952194db7594c7d8461c1fa3c992a80d994
Tishyaketu Deshpande <24838965+Tishyaketu@users.noreply.github.com>
2025-05-26 12:27:28 -0700

    Time: 0 ms (100.00%) | Memory: 19.1 MB (36.35%) - LeetSync

--- 261-graph-valid-tree/graph-valid-tree.py
+++ 261-graph-valid-tree/graph-valid-tree.py
@@ -1,17 +1,25 @@
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        if len(edges)!=n-1: return False
-        adj = [[] for _ in range(n)]
-        for A,B in edges:
-          adj[A].append(B)
-          adj[B].append(A)
-        parDict = {0:-1}
-        q = deque([0])
-        while q:
-          node = q.popleft()
-          for nbr in adj[node]:
-            if nbr==parDict[node]: continue
-            if nbr in parDict: return False
-            parDict[nbr] = node
-            q.append(nbr)
-        return len(parDict)==n
+        
+        if len(edges) != n - 1: return False
+        
+        # Create an adjacency list.
+        adj_list = [[] for _ in range(n)]
+        for A, B in edges:
+            adj_list[A].append(B)
+            adj_list[B].append(A)
+        
+        # We still need a seen set to prevent our code from infinite
+        # looping if there *is* cycles (and on the trivial cycles!)
+        seen = {0}
+        queue = collections.deque([0])
+        
+        while queue:
+            node = queue.popleft()
+            for neighbour in adj_list[node]:
+                if neighbour in seen:
+                    continue
+                seen.add(neighbour)
+                queue.append(neighbour)
+        
+        return len(seen) == n
