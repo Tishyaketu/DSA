c788a2d20c73946dd269ef260c8b953cc3181a02
Tishyaketu Deshpande <24838965+Tishyaketu@users.noreply.github.com>
2025-07-08 19:27:37 -0700

    Time: 63 ms (18.05%) | Memory: 21 MB (42.39%) - LeetSync

--- 285-inorder-successor-in-bst/inorder-successor-in-bst.py
+++ 285-inorder-successor-in-bst/inorder-successor-in-bst.py
@@ -1,22 +1,15 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
 class Solution:
-    def inOrder(self,node: TreeNode, arr: List[TreeNode]) -> List[TreeNode]:
-      if node.left==None and node.right==None:
-        arr.append(node)
-        return arr
-      if node.left is not None:
-        arr = self.inOrder(node.left,arr)
-      arr.append(node)
-      if node.right is not None:
-        arr = self.inOrder(node.right,arr)
-      return arr
-    def inorderSuccessor(self, root: TreeNode, p: TreeNode) -> Optional[TreeNode]:
-        inOrderArray = self.inOrder(root,[])
-        for i in range(len(inOrderArray)):
-          if inOrderArray[i] == p and i!=len(inOrderArray)-1: return inOrderArray[i+1]
-        return None
+    
+    def inorderSuccessor(self, root: 'TreeNode', p: 'TreeNode') -> 'TreeNode':
+        
+        successor = None
+        
+        while root:
+            
+            if p.val >= root.val:
+                root = root.right
+            else:
+                successor = root
+                root = root.left
+                
+        return successor
