3e83b059c2720d58df3c3e9a01dcf9d7e848160d
Tishyaketu Deshpande <24838965+Tishyaketu@users.noreply.github.com>
2025-07-14 12:13:17 -0700

    Time: 3 ms (92.17%) | Memory: 19 MB (62.71%) - LeetSync

--- 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.py
+++ 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.py
@@ -1,25 +1,23 @@
 class Solution:
-    def dfs(self, adj_list, visited, src):
-        visited[src] = True
-        for neighbor in adj_list[src]:
-            if not visited[neighbor]:
-                self.dfs(adj_list, visited, neighbor)
-    
-    def countComponents(self, n: int, edges: list[list[int]]) -> int:
-        if n == 0:
-            return 0
-        
-        components = 0
-        visited = [False] * n
-        adj_list = [[] for _ in range(n)]
-        
+    def dfs(self,node,visited,adj):
+      for nbr in adj[node]:
+        if nbr not in visited:
+          visited.add(nbr)
+          self.dfs(nbr,visited,adj)
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        adj = [[] for _ in range(n)]
         for edge in edges:
-            adj_list[edge[0]].append(edge[1])
-            adj_list[edge[1]].append(edge[0])
+          adj[edge[0]].append(edge[1])
+          adj[edge[1]].append(edge[0])
+        visited = set()
+        cc = 0
+        # for every node in adj list
+        for node in range(len(adj)):
+        # if node not visited start dfs for the node
+          if node not in visited:
+            visited.add(node)
+            self.dfs(node,visited,adj)
+            # once node and its nbrs are visited increment connected components count
+            cc+=1
+        return cc
         
-        for i in range(n):
-            if not visited[i]:
-                components += 1
-                self.dfs(adj_list, visited, i)
-        
-        return components
