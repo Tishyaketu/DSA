e9d72bbbace6103a469cf49b16819317feb21c4a
Tishyaketu Deshpande <24838965+Tishyaketu@users.noreply.github.com>
2025-07-17 13:29:08 -0700

    Time: 0 ms (100.00%) | Memory: 19.9 MB (48.23%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/meeting-rooms-ii.py
@@ -1,14 +1,14 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-      if not intervals: return 0
-      start_times = sorted([i[0] for i in intervals])
-      end_times = sorted(i[1] for i in intervals)
-      L = len(intervals)
-      ur, stp, etp = 0, 0, 0
-      while stp<L:
-        if start_times[stp]>=end_times[etp]:
-          ur-=1
-          etp+=1
-        ur+=1
-        stp+=1
-      return ur
+        wR = 0
+        st = sorted([i[0] for i in intervals])
+        et = sorted(i[1] for i in intervals)
+        stp,etp = 0, 0
+        L = len(intervals)
+        while stp<L:
+          if st[stp]>=et[etp]:
+            wR-=1
+            etp+=1
+          wR+=1
+          stp+=1
+        return wR
