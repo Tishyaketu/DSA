830b1127b08d17b239dc46654cac6105162c8dfb
Tishyaketu Deshpande <24838965+Tishyaketu@users.noreply.github.com>
2025-07-28 22:48:41 -0700

    Time: 61 ms (95.13%) | Memory: 19 MB (44.81%) - LeetSync

--- 761-employee-free-time/employee-free-time.py
+++ 761-employee-free-time/employee-free-time.py
@@ -1,21 +1,21 @@
 from typing import List
-
 class Interval:
     def __init__(self, start: int, end: int):
         self.start = start
         self.end = end
-
 class Solution:
     def employeeFreeTime(self, schedule: List[List[Interval]]) -> List[Interval]:
         result = []
         if not schedule:
             return result
-
-        # Flatten and sort intervals by start time
-        flats = sorted([iv for employee in schedule for iv in employee], key=lambda x: x.start)
-
+        # Flatten the schedule  
+        flats = []
+        for employee in schedule:
+          for interval in employee:
+            flats.append(interval)
+        # Sort the flattened list by interval start time
+        flats.sort(key=lambda x: x.start)
         temp = flats[0]
-
         for i in range(1, len(flats)):
             cur = flats[i]
             if cur.start <= temp.end:
@@ -23,5 +23,4 @@ class Solution:
             else:
                 result.append(Interval(temp.end, cur.start))
                 temp = cur
-
         return result
