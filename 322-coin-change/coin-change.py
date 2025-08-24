3c957646fa7ffb5f4a94ec2c8206ca39aa1bcbf3
Tishyaketu Deshpande <24838965+Tishyaketu@users.noreply.github.com>
2025-08-24 14:19:07 -0700

    Time: 316 ms (97.75%) | Memory: 18.7 MB (34.11%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,24 +1,14 @@
-from collections import deque
 class Solution:
-
-  def coinChange(self,coins, amount):
-    if amount == 0:
-        return 0
-
-    # queue holds tuples: (remaining_amount, number_of_coins_used)
-    queue = deque([(amount, 0)])
-    visited = set([amount])  # to avoid revisiting the same amount
-
-    while queue:
-        remaining, steps = queue.popleft()
-
-        # Try every coin
-        for coin in coins:
-            next_amount = remaining - coin
-            if next_amount == 0:
-                return steps + 1  # Found solution
-            if next_amount > 0 and next_amount not in visited:
-                visited.add(next_amount)
-                queue.append((next_amount, steps + 1))
-
-    return -1  # Not possible
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        if amount == 0: return 0
+        q = deque([(amount,0)])
+        v = set([amount])
+        while q:
+          rem, c = q.popleft()
+          for coin in coins:
+            nxt_amt = rem - coin
+            if nxt_amt == 0 : return c + 1
+            if nxt_amt > 0 and nxt_amt not in v:
+              v.add(nxt_amt)
+              q.append((nxt_amt,c+1))
+        return -1
