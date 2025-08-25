f28b546f42d2f88a73e783ff8ce657faf34bec79
Tishyaketu Deshpande <24838965+Tishyaketu@users.noreply.github.com>
2025-08-25 13:24:17 -0700

    Time: 323 ms (97.61%) | Memory: 18.7 MB (32.81%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,14 +1,14 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        if amount == 0: return 0
+        if amount == 0 : return 0
         q = deque([(amount,0)])
         v = set([amount])
         while q:
           rem, c = q.popleft()
           for coin in coins:
-            nxt_amt = rem - coin
-            if nxt_amt == 0 : return c + 1
-            if nxt_amt > 0 and nxt_amt not in v:
-              v.add(nxt_amt)
-              q.append((nxt_amt,c+1))
+            nxt = rem - coin
+            if nxt == 0 : return c + 1
+            if nxt > 0 and nxt not in v :
+              v.add(nxt)
+              q.append((nxt,c+1))
         return -1
