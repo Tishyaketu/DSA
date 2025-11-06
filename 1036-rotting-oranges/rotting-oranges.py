a9ccbbd3b30da34e9138556734cfa50d1847f2d5
Tishyaketu Deshpande <24838965+Tishyaketu@users.noreply.github.com>
2025-11-06 12:49:47 -0800

    Time: 4 ms (46.92%) | Memory: 17.9 MB (25.73%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,25 +1,22 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        a, b, c, d, e = 0, -1, deque([]), len(grid), len(grid[0])
-        for f in range(d):
-          for g in range(e):
-            if grid[f][g]==1: a += 1
-            elif grid[f][g]==2: 
-              c.append((f,g))
-        if a == 0: return 0
-        c.append((-1,-1))
-        while c:
-          h, i = c.popleft()
-          if h == -1:
-            b += 1
-            if c:
-              c.append((-1,-1))
-          else:
-            for j,k in [(0,1),(1,0),(0,-1),(-1,0)]:
-              l, m = h+j, i+k
-              if l>=0 and l<=d-1 and m>=0 and m<=e-1:
-                if grid[l][m]==1:
-                  grid[l][m] = 2
-                  a -= 1
-                  c.append((l,m))
-        return b if a==0 else -1
+        fresh, lvl, m, n, q = 0, -1, len(grid), len(grid[0]), deque([])
+        for i in range(m):
+          for j in range(n):
+            if grid[i][j] == 1: fresh += 1
+            elif grid[i][j] == 2: q.append((i,j))
+        if fresh == 0: return 0
+        q.append((-1,-1))
+        while q:
+          r, c = q.popleft()
+          if r == -1: 
+            lvl += 1
+            if q: q.append((-1,-1))
+          for x, y in [(1,0),(-1,0),(0,1),(0,-1)]:
+            newr, newc = r + x, c + y
+            if 0<= newr <=m-1 and 0<= newc <=n-1 and grid[newr][newc] == 1:
+              grid[newr][newc] = 2
+              fresh -= 1
+              q.append((newr, newc))
+        return lvl if fresh == 0 else -1
+
