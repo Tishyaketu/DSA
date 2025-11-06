7b3006005238f9c06fd92d84c9886e4de9388d4f
Tishyaketu Deshpande <24838965+Tishyaketu@users.noreply.github.com>
2025-11-06 12:50:55 -0800

    Time: 3 ms (81.43%) | Memory: 17.8 MB (71.20%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,6 +1,6 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        fresh, lvl, m, n, q = 0, -1, len(grid), len(grid[0]), deque([])
+        fresh, lvl, m, n, q, seen = 0, -1, len(grid), len(grid[0]), deque([]), set()
         for i in range(m):
           for j in range(n):
             if grid[i][j] == 1: fresh += 1
@@ -14,7 +14,7 @@ class Solution:
             if q: q.append((-1,-1))
           for x, y in [(1,0),(-1,0),(0,1),(0,-1)]:
             newr, newc = r + x, c + y
-            if 0<= newr <=m-1 and 0<= newc <=n-1 and grid[newr][newc] == 1:
+            if (newr,newc) not in seen and 0<= newr <=m-1 and 0<= newc <=n-1 and grid[newr][newc] == 1:
               grid[newr][newc] = 2
               fresh -= 1
               q.append((newr, newc))
