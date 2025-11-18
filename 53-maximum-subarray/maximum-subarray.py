f284f9aebc3830576e32af1fb89e69f0b6320ac6
Tishyaketu Deshpande <24838965+Tishyaketu@users.noreply.github.com>
2025-11-17 19:04:22 -0800

    Time: 104 ms (8.55%) | Memory: 32.6 MB (43.11%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -2,6 +2,6 @@ class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         cumulativeSum = maxSum = nums[0]
         for num in nums[1:]:
-          cumulativeSum = max(num,num+cumulativeSum)
-          maxSum = max(cumulativeSum,maxSum)
+          cumulativeSum = max(num,num+cumulativeSum) #only choose maximum value of the current element or the cumulative sum till the element
+          maxSum = max(cumulativeSum,maxSum) # only choose the global maximum not only the cumulative value
         return maxSum
